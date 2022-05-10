9b78e9bad3edd1e6e9d734009f184a5ffcc0f4dd
nanuro45 <62100388+nanuro45@users.noreply.github.com>
2022-05-10 19:34:06 -0300

    Update ModificarClienteUseCase.cs

--- TPintegrador/Biblioteca/UseCases/ModificarClienteUseCase.cs
+++ TPintegrador/Biblioteca/UseCases/ModificarClienteUseCase.cs
@@ -1,1 +1,17 @@
+using System;
+using ClasesInterfaces;
 
+namespace Biblioteca{
+    public class ModificarClienteUseCase: RepositorioClienteArchTexto{
+        public void Ejecutar(Cliente cliente){
+        try {
+            if(GetCliente(cliente.documento) != null){
+                ModificarCliente(cliente);
+            }
+        }
+        catch(Exception e){
+            Console.WriteLine(e.Message);
+        }
+      }
+    }
+}
