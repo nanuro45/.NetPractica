af0031cee5f2cff7cf04af97cfd177f74ff2c022
Aleeproia <62100665+Aleeproia@users.noreply.github.com>
2022-05-10 19:48:47 -0300

    Update AgregarEmpleadoUseCase.cs

--- TPintegrador/Biblioteca/UseCases/AgregarEmpleadoUseCase.cs
+++ TPintegrador/Biblioteca/UseCases/AgregarEmpleadoUseCase.cs
@@ -1,1 +1,19 @@
+namespace Biblioteca{
 
+    class AgregarEmpleadoUseCases:RepositorioEmpleadoArchTexto{
+
+        void Ejecutar(Empleado empleado){
+            try{
+                if(GetEmpleado(empleado.documento)!= null){
+                    AgregarEmpleado(empleado);
+                }
+            }      
+            catch(Exception e){
+                    Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+
+
+}
