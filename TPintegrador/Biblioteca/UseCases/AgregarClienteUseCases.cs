33d7690aedaf9d4806b60e1c0383c6a6b813a264
Aleeproia <62100665+Aleeproia@users.noreply.github.com>
2022-05-10 19:48:26 -0300

    Update AgregarClienteUseCases.cs

--- TPintegrador/Biblioteca/UseCases/AgregarClienteUseCases.cs
+++ TPintegrador/Biblioteca/UseCases/AgregarClienteUseCases.cs
@@ -1,1 +1,19 @@
+namespace Biblioteca{
+    class AgregarClienteUseCase:RepositorioClienteArchTexto{
 
+        void Ejecutar(Cliente cliente){
+            try{
+                if(GetCliente(cliente.documento)!= null){
+                    AgregarCliente(cliente);
+                }
+            }      
+            catch(Exception e){
+                    Console.WriteLine(e.Message);
+            }
+        }
+
+
+
+
+    }
+}
