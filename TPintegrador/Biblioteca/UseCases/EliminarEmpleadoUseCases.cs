3124acde497714a4c385682b9ee79b88b7967702
Aleeproia <62100665+Aleeproia@users.noreply.github.com>
2022-05-10 19:49:23 -0300

    Update EliminarEmpleadoUseCases.cs

--- TPintegrador/Biblioteca/UseCases/EliminarEmpleadoUseCases.cs
+++ TPintegrador/Biblioteca/UseCases/EliminarEmpleadoUseCases.cs
@@ -1,1 +1,15 @@
+namespace Biblioteca{
 
+    class EliminarEmpleadoUseCases:RepositorioEmpleadoArchTexto{
+        void Ejecutar(int dni){
+            try{
+                if(GetEmpleado(dni)!= null){
+                    EliminarEmpleado(dni);
+                }
+            }   
+            catch(Exception e){
+                    Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
