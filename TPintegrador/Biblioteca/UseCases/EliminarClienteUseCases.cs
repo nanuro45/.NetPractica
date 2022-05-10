aab6e8b502a1d0bce4f8f3aa8fae6dcf9cb5749a
Aleeproia <62100665+Aleeproia@users.noreply.github.com>
2022-05-10 19:49:04 -0300

    Update EliminarClienteUseCases.cs

--- TPintegrador/Biblioteca/UseCases/EliminarClienteUseCases.cs
+++ TPintegrador/Biblioteca/UseCases/EliminarClienteUseCases.cs
@@ -1,1 +1,20 @@
+namespace Biblioteca{
 
+    class EliminarClienteUseCases:RepositorioClienteArchTexto{
+
+
+        void Ejecutar(int dni){
+            try{
+                if(GetCliente(dni)!= null){
+                    EliminarCliente(dni);
+                }
+            }   
+            catch(Exception e){
+                    Console.WriteLine(e.Message);
+            }
+        }
+
+
+    }
+
+}
