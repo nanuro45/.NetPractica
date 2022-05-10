eed4211939ca3d671a8426d9a02355fe8fdb1643
nanuro45 <62100388+nanuro45@users.noreply.github.com>
2022-05-10 19:36:21 -0300

    Update ModificarEmpleadoUseCase.cs

--- TPintegrador/Biblioteca/UseCases/ModificarEmpleadoUseCase.cs
+++ TPintegrador/Biblioteca/UseCases/ModificarEmpleadoUseCase.cs
@@ -1,1 +1,18 @@
 
+using System;
+using ClasesInterfaces;
+
+namespace Biblioteca{
+    public class ModificarEmpleadoUseCase: RepositorioEmpleadoArchTexto{
+        public void Ejecutar(Empleado empleado){
+        try {
+            if(GetEmpleado(empleado.documento) != null){
+                ModificarEmpleado(empleado);
+            }
+        }
+        catch(Exception e){
+            Console.WriteLine(e.Message);
+        }
+      }
+    }
+}
