96b713697b3acd37c88944fb3ec5910a43da8dad
Aleeproia <62100665+Aleeproia@users.noreply.github.com>
2022-05-12 18:04:29 -0300

    Update ModificarClienteUseCase.cs

--- TPintegrador/Biblioteca/UseCases/ModificarClienteUseCase.cs
+++ TPintegrador/Biblioteca/UseCases/ModificarClienteUseCase.cs
@@ -1,12 +1,15 @@
-using System;
-using ClasesInterfaces;
-
 namespace Biblioteca{
-    public class ModificarClienteUseCase: RepositorioClienteArchTexto{
+    public class ModificarClienteUseCase{
+
+        RepositorioClienteArchTexto rc = new RepositorioClienteArchTexto();
         public void Ejecutar(Cliente cliente){
         try {
-            if(GetCliente(cliente.documento) != null){
-                ModificarCliente(cliente);
+            if(rc.GetCliente(cliente.documento) != null){
+                rc.ModificarCliente(cliente);
+                Console.WriteLine("*****Cliente modificado exitosamente *****");
+            }
+            else{
+                throw new Exception("Cliente no encontrado en el archivo");
             }
         }
         catch(Exception e){
